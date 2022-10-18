6e211480e1272f6a65cdfb8df099f7942326ac0f
hamidreza <takloohamidreza@gmail.com>
2022-10-18 22:03:05 +0330

    fix: Set text of all label center

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,38 +8,38 @@ function Form() {
         no file chosen
       </label>
       <input type="file" id="cv-image" className="hidden" />
-      <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"f-name"} className="text-stone-500">
+      <div className=" inline-flex flex-col  mx-4">
+        <label htmlFor={"f-name"} className="text-stone-500 text-center">
           First Name
         </label>
         <input type="text" id="f-name" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"l-name"} className="text-stone-500">
+        <label htmlFor={"l-name"} className="text-stone-500 text-center">
           Last Name
         </label>
         <input type="text" id="l-name" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"title"} className="text-stone-500">
+        <label htmlFor={"title"} className="text-stone-500 text-center">
           Title
         </label>
         <input type="text" id="title" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"phone"} className="text-stone-500">
+        <label htmlFor={"phone"} className="text-stone-500 text-center">
           phone Number
         </label>
         <input type="number" id="phone" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"email"} className="text-stone-500">
+        <label htmlFor={"email"} className="text-stone-500 text-center">
           Email Address
         </label>
         <input type="email" id="email" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor="address" className="text-stone-500">
+        <label htmlFor="address" className="text-stone-500 text-center">
           Address
         </label>
         <textarea
