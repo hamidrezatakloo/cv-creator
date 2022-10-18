a6584b8328dbfcdd17e962dca9de8b40115f32ee
hamidreza <takloohamidreza@gmail.com>
2022-10-18 19:42:27 +0330

    feat: create input with getting cv-image
    note: this input has different structure with Input component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,16 @@
+import Input from "./Input";
 function Form() {
-  return <form className="col-span-6"></form>;
+  return (
+    <form className="col-span-6">
+      <label
+        htmlFor="cv-image"
+        className="block w-[150px] h-[150px] bg-gray-300 flex justify-center items-center px-4 py-4 cursor-pointer"
+      >
+        no file chosen
+      </label>
+      <input type="file" id="cv-image" className="hidden" />
+    </form>
+  );
 }
 
 export default Form;
