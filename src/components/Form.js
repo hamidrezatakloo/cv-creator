7e6bc70fd0de3b881419e0b1ff1d4e0fb63ca8ac
hamidreza <takloohamidreza@gmail.com>
2022-10-19 00:52:28 +0330

    fix: Add Education and Experience to Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,14 @@
 import Info from "./Info";
-
+import Experience from "./Experience";
+import Education from "./Education";
 function Form() {
-  return <Info />;
+  return (
+    <div className="col-span-6">
+      <Info />
+      <Experience />
+      <Education />
+    </div>
+  );
 }
 
 export default Form;
