d66762a5c77f0f6031c380a3238efe313f8d0971
hamidreza <takloohamidreza@gmail.com>
2022-10-19 00:54:08 +0330

    fix: Remove text-center of label from components

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,31 +5,31 @@ function Experience(props) {
       <h2 className="mx-4 my-6">Experience</h2>
       <form className="grid grid-cols-12">
         <div className=" inline-flex flex-col  mx-4 col-span-4">
-          <label htmlFor={"position"} className="text-stone-500 text-center">
+          <label htmlFor={"position"} className="text-stone-500 ">
             Position
           </label>
           <input type="text" id="position" className="bg-gray-100" />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-4">
-          <label htmlFor={"company"} className="text-stone-500 text-center">
+          <label htmlFor={"company"} className="text-stone-500 ">
             Company
           </label>
           <input type="text" id="company" className="bg-gray-100" />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-4">
-          <label htmlFor={"city"} className="text-stone-500 text-center">
+          <label htmlFor={"city"} className="text-stone-500 ">
             City
           </label>
           <input type="text" id="city" className="bg-gray-100" />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
-          <label htmlFor={"from"} className="text-stone-500 text-center">
+          <label htmlFor={"from"} className="text-stone-500 ">
             From
           </label>
           <input type="number" id="from" className="bg-gray-100" />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
-          <label htmlFor={"to"} className="text-stone-500 text-center">
+          <label htmlFor={"to"} className="text-stone-500 ">
             To
           </label>
           <input type="email" id="to" className="bg-gray-100" />
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,45 +1,45 @@
 function Info(props) {
   return (
-    <form className="col-span-6 grid grid-cols-3">
+    <form className="grid grid-cols-3">
       <label
         htmlFor="cv-image"
-        className="block row-span-2 bg-gray-300 flex justify-center items-center px-4 py-4 cursor-pointer"
+        className="block row-span-2 bg-gray-300 flex justify-center items-center px-4 py-4 cursor-pointer mx-4"
       >
         no file chosen
       </label>
       <input type="file" id="cv-image" className="hidden" />
       <div className=" inline-flex flex-col  mx-4">
-        <label htmlFor={"f-name"} className="text-stone-500 text-center">
+        <label htmlFor={"f-name"} className="text-stone-500 ">
           First Name
         </label>
         <input type="text" id="f-name" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"l-name"} className="text-stone-500 text-center">
+        <label htmlFor={"l-name"} className="text-stone-500 ">
           Last Name
         </label>
         <input type="text" id="l-name" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-2">
-        <label htmlFor={"title"} className="text-stone-500 text-center">
+        <label htmlFor={"title"} className="text-stone-500 ">
           Title
         </label>
         <input type="text" id="title" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4">
-        <label htmlFor={"phone"} className="text-stone-500 text-center">
+        <label htmlFor={"phone"} className="text-stone-500 ">
           phone Number
         </label>
         <input type="number" id="phone" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-2">
-        <label htmlFor={"email"} className="text-stone-500 text-center">
+        <label htmlFor={"email"} className="text-stone-500 ">
           Email Address
         </label>
         <input type="email" id="email" className="bg-gray-100" />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-3">
-        <label htmlFor="address" className="text-stone-500 text-center">
+        <label htmlFor="address" className="text-stone-500 ">
           Address
         </label>
         <textarea
