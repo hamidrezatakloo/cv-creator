3249eb19afd1a376c43f114e4195b4c75a8627d2
hamidreza <takloohamidreza@gmail.com>
2022-10-20 00:13:03 +0330

    fix: store form values in block context
    for both Education and Experience component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,31 @@
-import { Fragment } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
+import { blockContext } from "./../App";
+import uniqid from "uniqid";
 function Education(props) {
+  const context = useContext(blockContext);
+  const [data, setData] = useState({
+    id: uniqid(),
+    title: "Education",
+    university: "",
+    degree: "",
+    city: "",
+    subject: "",
+    from: "",
+    to: "",
+  });
+  useEffect(() => {
+    // add this instance state to blocks context
+    context.AddBlocks(data);
+  }, []);
+
+  const handleInput = (e) => {
+    const id = e.target.getAttribute("id");
+    setData(...[data], (data[id] = e.target.value));
+    const temp = context.blocks.map((obj) =>
+      obj.id === data.id ? { ...data } : obj
+    );
+    context.updateBlocks(temp);
+  };
   return (
     <Fragment>
       <h2 className="mx-4 my-6">Education</h2>
@@ -8,37 +34,67 @@ function Education(props) {
           <label htmlFor={"university"} className="text-stone-500 ">
             University Name
           </label>
-          <input type="text" id="university" className="bg-gray-100" />
+          <input
+            type="text"
+            id="university"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"degree"} className="text-stone-500 ">
             Degree
           </label>
-          <input type="text" id="degree" className="bg-gray-100" />
+          <input
+            type="text"
+            id="degree"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"city"} className="text-stone-500 ">
             City
           </label>
-          <input type="text" id="city" className="bg-gray-100" />
+          <input
+            type="text"
+            id="city"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"subject"} className="text-stone-500 ">
             Subject
           </label>
-          <input type="text" id="subject" className="bg-gray-100" />
+          <input
+            type="text"
+            id="subject"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"from"} className="text-stone-500 ">
             From
           </label>
-          <input type="number" id="from" className="bg-gray-100" />
+          <input
+            type="number"
+            id="from"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"to"} className="text-stone-500 ">
             To
           </label>
-          <input type="email" id="to" className="bg-gray-100" />
+          <input
+            type="email"
+            id="to"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
       </form>
     </Fragment>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,30 @@
-import { Fragment } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
+import { blockContext } from "./../App";
+import uniqid from "uniqid";
 function Experience(props) {
+  const context = useContext(blockContext);
+  const [data, setData] = useState({
+    id: uniqid(),
+    title: "Experience",
+    position: "",
+    company: "",
+    city: "",
+    from: "",
+    to: "",
+  });
+  useEffect(() => {
+    // add this instance state to blocks context
+    context.AddBlocks(data);
+  }, []);
+
+  const handleInput = (e) => {
+    const id = e.target.getAttribute("id");
+    setData(...[data], (data[id] = e.target.value));
+    const temp = context.blocks.map((obj) =>
+      obj.id === data.id ? { ...data } : obj
+    );
+    context.updateBlocks(temp);
+  };
   return (
     <Fragment>
       <h2 className="mx-4 my-6">Experience</h2>
@@ -8,31 +33,56 @@ function Experience(props) {
           <label htmlFor={"position"} className="text-stone-500 ">
             Position
           </label>
-          <input type="text" id="position" className="bg-gray-100" />
+          <input
+            type="text"
+            id="position"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-4">
           <label htmlFor={"company"} className="text-stone-500 ">
             Company
           </label>
-          <input type="text" id="company" className="bg-gray-100" />
+          <input
+            type="text"
+            id="company"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-4">
           <label htmlFor={"city"} className="text-stone-500 ">
             City
           </label>
-          <input type="text" id="city" className="bg-gray-100" />
+          <input
+            type="text"
+            id="city"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"from"} className="text-stone-500 ">
             From
           </label>
-          <input type="number" id="from" className="bg-gray-100" />
+          <input
+            type="number"
+            id="from"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
         <div className=" inline-flex flex-col mx-4 col-span-6">
           <label htmlFor={"to"} className="text-stone-500 ">
             To
           </label>
-          <input type="email" id="to" className="bg-gray-100" />
+          <input
+            type="email"
+            id="to"
+            className="bg-gray-100"
+            onChange={handleInput}
+          />
         </div>
       </form>
     </Fragment>
