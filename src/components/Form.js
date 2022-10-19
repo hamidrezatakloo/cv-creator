d5d4c8a4f23d3dee18346e103740c047f6df262a
hamidreza <takloohamidreza@gmail.com>
2022-10-19 23:34:33 +0330

    fix: set padding,border,boxShadow

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import Experience from "./Experience";
 import Education from "./Education";
 function Form() {
   return (
-    <div className="col-span-6">
+    <div className="col-span-6 border-2 border-gray-200 shadow-lg py-4">
       <Info />
       <Experience />
       <Education />
