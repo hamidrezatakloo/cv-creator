46b3a7c52df16d2cddea2e268eb176967b8ae458
hamidreza <takloohamidreza@gmail.com>
2022-10-19 12:33:27 +0330

    fix: Add Profile and history in view component

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,12 @@
+import Profile from "./Profile";
+import History from "../History";
 function View() {
-  return <div className="col-span-6"></div>;
+  return (
+    <div className="col-span-6 grid grid-cols-3 border-t-[40px] border-t-cyan-400">
+      <Profile />
+      <History />
+    </div>
+  );
 }
 
 export default View;
