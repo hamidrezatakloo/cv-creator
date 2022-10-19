c6d804c523300d75ca8ad92e561e1d97ab7ba1db
hamidreza <takloohamidreza@gmail.com>
2022-10-20 00:19:54 +0330

    feat: create components for show context value
    instead Block component we have 2 Education and Experience
    components

---
+++ src/components/EducationView.js
@@ -0,0 +1,24 @@
+function EducationView({ block }) {
+  return (
+    <div className="text-cyan-500 my-4">
+      <h1 className="text-lg font-bold">{block.title}</h1>
+      <div className="flex justify-between">
+        <h2 className="text-black text-xl font-bold">{block.subject}</h2>
+        <div>
+          <span>{block.from}</span>
+          {block.from ? "-" : ""}
+          <span>{block.to}</span>
+        </div>
+      </div>
+      <div>
+        <span>{block.university}</span>
+        {block.university ? "," : ""}
+        <span>{block.city}</span>
+        {block.city ? "," : ""}
+        <span>{block.degree}</span>
+      </div>
+    </div>
+  );
+}
+
+export default EducationView;
---
+++ src/components/ExperienceView.js
@@ -0,0 +1,22 @@
+function ExperienceView({ block }) {
+  return (
+    <div className="text-cyan-500 my-4">
+      <h1 className="text-lg font-bold">{block.title}</h1>
+      <div className="flex justify-between">
+        <h2 className="text-black text-xl font-bold">{block.position}</h2>
+        <div>
+          <span>{block.from}</span>
+          {block.from ? "-" : ""}
+          <span>{block.to}</span>
+        </div>
+      </div>
+      <div>
+        <span>{block.company}</span>
+        {block.company ? "," : ""}
+        <span>{block.city}</span>
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceView;
