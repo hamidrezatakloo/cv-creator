9ea10c54c84b4d201cb3bb915876e71090fc674b
hamidreza <takloohamidreza@gmail.com>
2022-10-19 13:16:24 +0330

    feat: Add Block component
    Block component is view of Experience or Education

---
+++ src/components/Block.js
@@ -0,0 +1,18 @@
+function Form() {
+  return (
+    <div className="text-cyan-500 my-4">
+      <h1 className="text-lg font-bold">Experience</h1>
+      <div className="flex justify-between">
+        <h2 className="text-black text-xl font-bold">Senior web developer</h2>
+        <div>
+          <span>2016</span>-<span>2018</span>
+        </div>
+      </div>
+      <div>
+        <span>facebook</span>,<span>inc</span>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
