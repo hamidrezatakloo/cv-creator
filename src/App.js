a9a109a8cd14b8e0abce66f80088b0b3077fd6c0
hamidreza <takloohamidreza@gmail.com>
2022-10-20 00:05:34 +0330

    feat: create context and pass to child components
    context is a object that have 3 value:
    blocks: a list of objects. this list contain state for
    AddBlock: add new object to list in child component
    UpdateBlock: update object list with new list
    Experience or Education component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Container from "./components/Container";
+import { createContext, useState } from "react";
+export const blockContext = createContext();
 function App() {
+  const [blocks, setBl] = useState([]);
+  const AddBlocks = (newBlock) => {
+    setBl((prevState) => [...prevState, newBlock]);
+  };
+  const updateBlocks = (data) => {
+    setBl(data);
+  };
   return (
-    <div className="App">
-      <Container />
-    </div>
+    <blockContext.Provider value={{ blocks, AddBlocks, updateBlocks }}>
+      <div className="App">
+        <Container />
+      </div>
+    </blockContext.Provider>
   );
 }
 
