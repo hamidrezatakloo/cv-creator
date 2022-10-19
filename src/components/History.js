98791ca7287f454249eccbec731d04a2ac26c10b
hamidreza <takloohamidreza@gmail.com>
2022-10-20 00:22:50 +0330

    fix: map component for each object in context list

--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,18 @@
+import ExperienceView from "./ExperienceView";
+import { useContext } from "react";
+import { blockContext } from "./../App";
+import EducationView from "./EducationView";
 function History() {
-  return <div className="col-span-2"></div>;
+  const context = useContext(blockContext);
+  return (
+    <div className="col-span-2 px-4">
+      {context.blocks.map((block, i) => {
+        if (block.title === "Experience")
+          return <ExperienceView block={block} key={i} />;
+        else return <EducationView block={block} key={i} />;
+      })}
+    </div>
+  );
 }
 
 export default History;
