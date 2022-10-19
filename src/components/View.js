d4ca7360138fcec66f73a6ce1c64f6c9bb1655fd
hamidreza <takloohamidreza@gmail.com>
2022-10-19 13:10:23 +0330

    fix: move history to component folder
    just correct mistake :)

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,5 @@
 import Profile from "./Profile";
-import History from "../History";
+import History from "./History";
 function View() {
   return (
     <div className="col-span-6 grid grid-cols-3 border-t-[40px] border-t-cyan-400">
