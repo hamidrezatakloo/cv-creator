69e10ca08e1b5ddfebf11cca924332e7877ce235
hamidreza <takloohamidreza@gmail.com>
2022-10-20 15:47:12 +0330

    fix: use info state for rendering to screen

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,23 +2,24 @@ import { useContext } from "react";
 import { blockContext } from "../App";
 function Profile() {
   const context = useContext(blockContext);
-
   return (
     <div className="flex flex-col">
       <div>
         <img src={context.src} alt="profile" className="w-[200px] rounded" />
         <h2 className="capitalize text-cyan-500 font-bold text-2xl">
-          senior developer
+          {context.info.title}
         </h2>
       </div>
       <h4>Name</h4>
-      <p className="text-stone-500">hamid reza</p>
+      <p className="text-stone-500">
+        {context.info["f-name"] + " " + context.info["l-name"]}
+      </p>
       <h4>Email Address</h4>
-      <p className="text-stone-500">gamemaster9970@gmail.com</p>
+      <p className="text-stone-500">{context.info.email}</p>
       <h4>Phone Number</h4>
-      <p className="text-stone-500">+98939166257</p>
+      <p className="text-stone-500">{context.info.phone}</p>
       <h4>Address</h4>
-      <p className="text-stone-500">emam ali highway tajik street cottage 14</p>
+      <p className="text-stone-500">{context.info.address}</p>
     </div>
   );
 }
