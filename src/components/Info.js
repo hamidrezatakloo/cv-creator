ddb2c5e5c30c5e86a73b326429ab56b6b7726a00
hamidreza <takloohamidreza@gmail.com>
2022-10-20 15:46:12 +0330

    fix: update info states with input values

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,10 +2,17 @@ import { useContext } from "react";
 import { blockContext } from "../App";
 function Info(props) {
   const context = useContext(blockContext);
-  console.log(context.src);
+
   const handleFileInput = (e) => {
     context.setSrc(URL.createObjectURL(e.target.files[0]));
   };
+
+  const handleInfo = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    context.setInfo({ ...context.info, [id]: value });
+  };
+
   return (
     <form className="grid grid-cols-3">
       <label
@@ -24,31 +31,56 @@ function Info(props) {
         <label htmlFor={"f-name"} className="text-stone-500 ">
           First Name
         </label>
-        <input type="text" id="f-name" className="bg-gray-100" />
+        <input
+          type="text"
+          id="f-name"
+          className="bg-gray-100"
+          onChange={handleInfo}
+        />
       </div>
       <div className=" inline-flex flex-col mx-4">
         <label htmlFor={"l-name"} className="text-stone-500 ">
           Last Name
         </label>
-        <input type="text" id="l-name" className="bg-gray-100" />
+        <input
+          type="text"
+          id="l-name"
+          className="bg-gray-100"
+          onChange={handleInfo}
+        />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-2">
         <label htmlFor={"title"} className="text-stone-500 ">
           Title
         </label>
-        <input type="text" id="title" className="bg-gray-100" />
+        <input
+          type="text"
+          id="title"
+          className="bg-gray-100"
+          onChange={handleInfo}
+        />
       </div>
       <div className=" inline-flex flex-col mx-4">
         <label htmlFor={"phone"} className="text-stone-500 ">
           phone Number
         </label>
-        <input type="number" id="phone" className="bg-gray-100" />
+        <input
+          type="number"
+          id="phone"
+          className="bg-gray-100"
+          onChange={handleInfo}
+        />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-2">
         <label htmlFor={"email"} className="text-stone-500 ">
           Email Address
         </label>
-        <input type="email" id="email" className="bg-gray-100" />
+        <input
+          type="email"
+          id="email"
+          className="bg-gray-100"
+          onChange={handleInfo}
+        />
       </div>
       <div className=" inline-flex flex-col mx-4 col-span-3">
         <label htmlFor="address" className="text-stone-500 ">
@@ -59,6 +91,7 @@ function Info(props) {
           rows="4"
           cols="50"
           className="bg-gray-100"
+          onChange={handleInfo}
         ></textarea>
       </div>
     </form>
