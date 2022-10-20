907eb1e9fafb11f384e018873bbf4b9b286c12f1
hamidreza <takloohamidreza@gmail.com>
2022-10-20 18:15:39 +0330

    feat: create new exp-edu component with Add button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,32 @@
 import Info from "./Info";
 import Experience from "./Experience";
 import Education from "./Education";
+import { useState } from "react";
 function Form() {
+  const [exp, setExp] = useState(1);
+  const [edu, setEdu] = useState(1);
   return (
     <div className="col-span-6 border-2 border-gray-200 shadow-lg py-4">
       <Info />
-      <Experience />
-      <Education />
+      {[...Array(exp)].map((v, i) => (
+        <Experience key={i} />
+      ))}
+      <button
+        className="w-full mt-4 bg-emerald-300 text-white text-lg font-medium py-1 rounded"
+        onClick={() => setExp(exp + 1)}
+      >
+        Add
+      </button>
+
+      {[...Array(edu)].map((v, i) => (
+        <Education key={i} />
+      ))}
+      <button
+        className="w-full mt-4 bg-emerald-300 text-white text-lg font-medium py-1 rounded"
+        onClick={() => setEdu(edu + 1)}
+      >
+        Add
+      </button>
     </div>
   );
 }
