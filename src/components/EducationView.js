6ecbafa775fa8483484b66022ccfe9b9ffe2ee3f
hamidreza <takloohamidreza@gmail.com>
2022-10-21 12:57:39 +0330

    style: Add vertical margin

--- src/components/EducationView.js
+++ src/components/EducationView.js
@@ -2,7 +2,7 @@ function EducationView({ block }) {
   return (
     <div className="text-cyan-500 my-4">
       <h1 className="text-lg font-bold">{block.title}</h1>
-      <div className="flex justify-between">
+      <div className="flex justify-between my-4">
         <h2 className="text-black text-xl font-bold">{block.subject}</h2>
         <div>
           <span>{block.from}</span>
--- src/components/ExperienceView.js
+++ src/components/ExperienceView.js
@@ -2,7 +2,7 @@ function ExperienceView({ block }) {
   return (
     <div className="text-cyan-500 my-4">
       <h1 className="text-lg font-bold">{block.title}</h1>
-      <div className="flex justify-between">
+      <div className="flex justify-between my-4">
         <h2 className="text-black text-xl font-bold">{block.position}</h2>
         <div>
           <span>{block.from}</span>
